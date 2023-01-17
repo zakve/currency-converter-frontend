87125f4cbebac78d8e48f3340e27dcc993d6dff6
zakve <zaklas.martin@gmail.com>
2023-01-17 18:44:31 +0100

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const Footer = () => (
+    <footer>
+        <Container
+            maxWidth={false}
+            sx={{
+                bgcolor: 'primary.main',
+                textAlign: 'center',
+                padding: '1rem'
+            }}
+        >
+            <Typography
+                sx={{
+                    color: '#ffffff'
+                }}
+            >
+                Martin Zaklasnik 2023
+            </Typography>
+        </Container>
+    </footer>
+);
+
+export default Footer;
