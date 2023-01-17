befecf864e45f03f441b550f90e6346fdffa646d
zakve <zaklas.martin@gmail.com>
2023-01-17 18:44:57 +0100

    Add container and components to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Container from '@mui/material/Container';
+import ResponsiveAppBar from './components/AppBarResponsive';
+import Footer from './components/Footer';
+import ConvertBox from './containers/ConvertBox';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <ResponsiveAppBar />
-      </header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveAppBar />
+      <main>
+        <Container maxWidth="xl">
+          <ConvertBox />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
